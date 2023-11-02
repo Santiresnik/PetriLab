0849966f7047e2e90a9e0eab4b9d85decd9c7a4d
SantiResnik <79484050+Santiresnik@users.noreply.github.com>
2023-11-01 21:26:49 -0300

    trying not express
    changing index.js ia code to next

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,32 +1,17 @@
-const mysql = require("mysql2");
-const dotenv = require("dotenv");
+import { NextResponse } from "next/server"
+
+
+export async function GET() {
+  return NextResponse.json("Holaaaa")
+}
+
+/*
 const axios = require('axios');
-const cors = require('cors');
 const https = require('https'); // Import the 'https' module
-const express = require('express');
-dotenv.config();
 
-const app = express();
 
-// Middleware for handling CORS using the cors middleware
-const corsOptions = {
-  origin: '*',
-  credentials: true,
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  allowedHeaders: 'Origin, X-Requested-With, Content-Type, Accept, Authorization',
-};
+export async function GET(req: Request, { params: { id } }: Props) {
 
-// app.use(cors());
-
-// Middleware for parsing JSON requests
-app.use(express.json());
-
-// Define a route for the root
-app.get("/", (req, res) => {
-  res.send("Welcome to Planetscale API PetriLab!");
-});
-
-// Define a route for processing images
 app.get('/process_image', async (req, res) => {
   try {
     const imageUrl = req.body.image_url; // Assuming your JSON payload has an "image_url" field
@@ -55,12 +40,5 @@ app.get('/process_image', async (req, res) => {
   }
 });
 
-// Define a catch-all route for 404 errors
-app.use("*", (req, res) => {
-  res.status(404).send("404 - Not Found");
-});
-
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log("Server is running on port", port);
-});
+}
+*/
